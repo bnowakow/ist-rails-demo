4f1892dcd2f01e134896589217848ebb0a127928
Bartek Nowakowski <bartek@bnowakowski.pl>
2015-01-28 15:23:12 +0100

    Add tracking_number to Order

--- rails-demo/db/schema.rb
+++ rails-demo/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128135745) do
+ActiveRecord::Schema.define(version: 20150128141904) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "number"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "tracking_number"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
